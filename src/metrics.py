87af4cb865cb3a0411f393a72d8e1bfa0d71905f
Tim-Ryd <tim.ryden@hotmail.se>
2025-10-14 12:29:59 +0200

    Justerade report location och la in top 3 kategori

--- src/metrics.py
+++ src/metrics.py
@@ -1,2 +1,6 @@
 #metrics.py = nyckeltals-funktioner
 
+def top_3_category_by_revenue(df):
+    top_3 = df.groupby('category')['revenue'].sum().reset_index().sort_values('revenue', ascending=False).head(3)
+
+    return top_3
