e3fcf123bef9f9bceddea269a1fc3b97be77ebff
salam8108 <salam.adel@hotmail.com>
2025-10-15 19:30:53 +0200

    updated report notebook and project structure

--- src/metrics.py
+++ src/metrics.py
@@ -11,3 +11,21 @@ def top_3_date_by_revenue(df):
     top_3 = df.groupby('date')['revenue'].sum().reset_index().sort_values('revenue', ascending=False).head(3)
 
     return top_3
+import pandas as pd
+
+def revenue_per_category(df):
+    """
+    Beräknar total intäkt per kategori.
+    
+    Argument:
+        df (DataFrame): DataFrame med kolumnerna 'category' och 'revenue'
+    
+    Returnerar:
+        DataFrame sorterad efter total intäkt per kategori (högst först)
+    """
+    revenue_df = (
+        df.groupby("category", as_index=False)["revenue"]
+        .sum()
+        .sort_values(by="revenue", ascending=False)
+    )
+    return revenue_df
